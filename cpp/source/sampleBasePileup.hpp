6514bda55bc9fa7d8f214eced56e103fd80d16ed
GrantDaly <gdaly9000@gmail.com>
2022-05-31 14:22:38 -0500

    updated variant pileups to calculate artifacts and strand check

--- cpp/source/sampleBasePileup.hpp
+++ cpp/source/sampleBasePileup.hpp
@@ -3,81 +3,46 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <math.h>
+
 class SampleBasePileup
 {
  public:
-  hts_pos_t pos = -1;
-  std::string contig = "";
-  char ref = 'N';
+  
+  // giving SampleBasePileup knowledge of which base this is
   // F1R2 Read 1's
-  std::map<std::string, long int> F1R2R1s = {
-    {"A", 0},
-    {"T", 0},
-    {"C", 0},
-    {"G", 0}
-  };
+  long int F1R2R1s = 0;
+  long int F1R2R2s = 0;
+  long int F2R1R1s = 0;
+  long int F2R1R2s = 0;
+  SampleBasePileup & operator=(SampleBasePileup &&) = default;
 
-  // F1R2 Read 2's
-    std::map<std::string, long int> F1R2R2s = {
-    {"A", 0},
-    {"T", 0},
-    {"C", 0},
-    {"G", 0}
-  };
-  
-  // F2R1 Read 1's
-    std::map<std::string, long int> F2R1R1s = {
-    {"A", 0},
-    {"T", 0},
-    {"C", 0},
-    {"G", 0}
-  };
-
-  // F2R1 Read 2's
-    std::map<std::string, long int> F2R1R2s = {
-    {"A", 0},
-    {"T", 0},
-    {"C", 0},
-    {"G", 0}
-  };
-  //SampleBasePileup(hts_pos_t in_pos,  std::string in_contig);
-  SampleBasePileup(hts_pos_t in_pos, std::string in_contig);
-  SampleBasePileup(hts_pos_t in_pos, std::string in_contig, char in_ref);
-  
-  friend std::ostream& operator<<(std::ostream& os, const SampleBasePileup& pl);
-
-  static void writeHeader(std::ostream & os){
-    os << "Chrom" << "\t" << "Start" << "\t" << "ref" << "\t" << "base" << "\t" <<
-      "F1R2R1" << "\t" << "F1R2R2" << "\t" << "F2R1R1" << "\t" << "F2R1R2" << std::endl;
-
-  };
-
- 
+  double artifactScore();
+  std::string strandTest();
+  long int totalBases();
 };
-// SampleBasePileup(hts_pos_t in_pos,  std::string in_contig){
-//     pos = in_pos;
-//     contig = in_contig;
-//   }
-// SampleBasePileup::SampleBasePileup(hts_pos_t in_pos,  std::string in_contig): pos(in_pos), contig(in_contig) {}
-SampleBasePileup::SampleBasePileup(hts_pos_t in_pos, std::string in_contig)
-    : pos(in_pos), contig(in_contig) {}
 
-SampleBasePileup::SampleBasePileup(hts_pos_t in_pos, std::string in_contig, char in_ref): pos(in_pos), contig(in_contig), ref(in_ref) {}
-std::ostream& operator<<(std::ostream& os, const SampleBasePileup& pl)
-  {
-    os << pl.contig << "\t" << pl.pos << "\t" << pl.ref << "\t" << "A" << "\t" << 
-      pl.F1R2R1s.at("A") << "\t" << pl.F1R2R2s.at("A") << "\t" <<
-      pl.F2R1R1s.at("A") << "\t" << pl.F2R1R2s.at("A") << std::endl  <<
-      pl.contig << "\t" << pl.pos << "\t" << pl.ref << "\t" << "T" << "\t" <<
-      pl.F1R2R1s.at("T") << "\t" << pl.F1R2R2s.at("T") << "\t" <<
-      pl.F2R1R1s.at("T") << "\t" << pl.F2R1R2s.at("T") << std::endl <<
-      pl.contig << "\t" << pl.pos << "\t" << pl.ref << "\t" << "C" << "\t" << 
-      pl.F1R2R1s.at("C") << "\t" << pl.F1R2R2s.at("C") << "\t" <<
-      pl.F2R1R1s.at("C") << "\t" << pl.F2R1R2s.at("C") << std::endl <<
-      pl.contig << "\t" << pl.pos << "\t" << pl.ref << "\t" << "G" << "\t" <<
-      pl.F1R2R1s.at("G") << "\t" << pl.F1R2R2s.at("G") << "\t" <<
-      pl.F2R1R1s.at("G") << "\t" << pl.F2R1R2s.at("G") << std::endl;
-                        
-    return os;
-
+/*
+  F1R2 R1 | F1R2 R2
+  -----------------
+  F2R1 R1 | F2R1 R2
+  Strand artifacts are diagonals (F1R2 R1 and F2R1 R2 for +) and (F1R2 R2 and F2R1 R1 for +)
+  Read orientation (pre-adapter) are (F1R2 R1 and R2 for +) and (F2R1 R1 and R2 for -)
+ */
+double SampleBasePileup::artifactScore() {
+  // log2 ratio of F1R2 to F2R1 to detect orientation artifacts (pre-adapter)
+  return log2( (double)(F1R2R1s + F1R2R2s + 1) / (F2R1R1s + F2R1R2s + 1) );
+}
+std::string SampleBasePileup::strandTest() {
+  // flag for if mimimum number of reads go to both strands to avoid sequencing artifacts
+  if(((F1R2R1s + F2R1R2s) >= 10) && ((F1R2R2s + F2R1R1s) >= 10)){
+    return std::string("True");
   }
+  else {
+    return std::string("False");
+  }
+}
+long int SampleBasePileup::totalBases() {
+  return F1R2R1s + F1R2R2s + F2R1R1s + F2R1R2s;
+}
+
